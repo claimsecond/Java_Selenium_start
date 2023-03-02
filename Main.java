6c457343997401d62dad7a2ef1a5ebbeee677935
claimsecond <claim.second@gmail.com>
2023-03-02 21:06:04 +0200

    Driver finds 1st element on the results page and click it

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 
 public class Main {
@@ -8,7 +13,13 @@ public class Main {
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
-
         driver.get("https://google.com/ncr");
+
+        WebElement input = driver.findElement(By.name("q"));
+        input.sendKeys("Selenium");
+        input.sendKeys(Keys.ENTER);
+
+        List<WebElement> elements = driver.findElements(By.cssSelector("a h3"));
+        elements.get(0).click();
     }
 }
